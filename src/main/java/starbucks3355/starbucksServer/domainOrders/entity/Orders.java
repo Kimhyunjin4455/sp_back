e21758d1e4a117945631f01859a40d249ce10eb0
daegwan00 <daegwanou@gmail.com>
2024-08-28 14:34:01 +0900

    주문 패키지의  엔티티 패키지 수정

--- src/main/java/starbucks3355/starbucksServer/domainOrders/entity/Orders.java
+++ src/main/java/starbucks3355/starbucksServer/domainOrders/entity/Orders.java
@@ -1,36 +1,38 @@
 package starbucks3355.starbucksServer.domainOrders.entity;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-
-import java.time.LocalDateTime;
-import java.util.UUID;
 
 @Entity
 @NoArgsConstructor
 @Getter
 @ToString
-@Table(name = "orders")
 public class Orders {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(nullable = false, length = 30)
-    private Long order_id;
-    @Column(nullable = false, length = 30)
-    private LocalDateTime order_date;
-    @Column(nullable = false, length = 40)
-    private Integer total_amount;
-    @Column(nullable = false, length = 100)
-    private UUID uu_id;
-    @Column(nullable = false, length = 30)
-    private String user_name;
-    @Column(nullable = false, length = 30)
-    private String user_phoneNumber;
-    @Column(nullable = false, length = 30)
-    private String user_address;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(nullable = false, length = 30)
+	private Long orderId;
+	@Column(nullable = false, length = 30)
+	private LocalDateTime orderDate;
+	@Column(nullable = false, length = 40)
+	private Integer totalAmount;
+	@Column(nullable = false, length = 100)
+	private UUID uuId;
+	@Column(nullable = false, length = 30)
+	private String userName;
+	@Column(nullable = false, length = 30)
+	private String userPhoneNumber;
+	@Column(nullable = false, length = 30)
+	private String userAddress;
 
 }

dd9ba49006c31cb5334bc9ccdade27a4687c7242
Kimhyunjin4455 <dp4455@naver.com>
2024-08-29 23:40:27 +0900

    fix: OrderDetails의 변수명으로 인한 git 충돌 해결

--- src/main/java/starbucks3355/starbucksServer/domainOrders/entity/OrderDetails.java
+++ src/main/java/starbucks3355/starbucksServer/domainOrders/entity/OrderDetails.java
@@ -24,7 +24,7 @@ public class OrderDetails {
 	private Long id;
 
 	@ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-	private Orders order;
+	private Orders orders;
 	@Column(nullable = false, length = 30)
 	private Integer productCnt;
 	@Column(nullable = false, length = 30)

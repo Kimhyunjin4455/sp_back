cb7938f5bf6942baed70b758a4027e122a220852
daegwan00 <daegwanou@gmail.com>
2024-09-01 23:26:31 +0900

    fix: 주문목록조회 mapping 방식 변경

--- src/main/java/starbucks3355/starbucksServer/domainOrders/controller/OrderController.java
+++ src/main/java/starbucks3355/starbucksServer/domainOrders/controller/OrderController.java
@@ -5,10 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import lombok.RequiredArgsConstructor;
 import starbucks3355.starbucksServer.domainOrders.dto.request.OrderRequestDto;
@@ -45,7 +42,7 @@ public class OrderController {
 	}
 
 	//주문 목록 조회
-	@PostMapping("/list")
+	@GetMapping("/list")
 	public ResponseEntity<List<OrderResponseVo>> getAllOrders() {
 		// 서비스에서 목록 가져오기
 		List<Orders> ordersList = orderService.getAllOrders();

9c495f50249fa8efe90e9f915b3255e318af6a85
joungoni <joungoni@gmail.com>
2024-09-03 15:38:59 +0900

    fix: 로그인, 로그아웃, 회원가입

--- src/main/java/starbucks3355/starbucksServer/domainMember/controller/MemberController.java
+++ src/main/java/starbucks3355/starbucksServer/domainMember/controller/MemberController.java
@@ -2,6 +2,8 @@ package starbucks3355.starbucksServer.domainMember.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,21 +32,14 @@ public class MemberController {
 
 	@PostMapping("/signup")
 	@Operation(summary = "회원가입")
-	//vo의 값을 Dto로 변환
+	// vo의 값을 Dto로 변환
 	// 변환된 값을 Service로 넘겨줌
-	public ResponseEntity<Member> signUpMember(MemberRequestVo memberRequestVo) {
+	public ResponseEntity<Member> signUpMember(@RequestBody MemberRequestVo memberRequestVo) {
 		MemberRequestDto memberRequestDto = new MemberRequestDto(
 			memberRequestVo.getUserId(),
 			memberRequestVo.getPassword(),
 			memberRequestVo.getEmail(),
-			memberRequestVo.getPhoneNumber(),
-			memberRequestVo.getBirth(),
-			memberRequestVo.getAddress(),
-			memberRequestVo.getGender(),
-			memberRequestVo.getNickname(),
-			memberRequestVo.getMemberUuid(),
-			memberRequestVo.getIsMember(),
-			memberRequestVo.getWithdrawalTime()
+			memberRequestVo.getNickname()
 		);
 		memberService.signUpMember(memberRequestDto);
 		return new ResponseEntity<Member>(HttpStatus.CREATED);
@@ -52,25 +47,31 @@ public class MemberController {
 
 	@PostMapping("/login")
 	@Operation(summary = "로그인")
-	public ResponseEntity<LoginResponseDto> loginMember(@RequestBody LoginRequestDto loginRequestDto) {
+	public ResponseEntity<LoginResponseDto> loginMember(@RequestBody LoginRequestDto loginRequestDto, HttpServletRequest request) {
 		// 로그인 서비스 호출하여 LoginResponseDto 객체 가져옴
 		LoginResponseDto loginResponseDto = memberService.loginMember(loginRequestDto);
 
+		// 로그인 성공시 세션에 사용자 정보 저장
+		HttpSession session = request.getSession();
+		session.setAttribute("user", loginRequestDto.getUserId()); // 예시로 사용자 ID 저장
 		// 로그인 성공
 		return new ResponseEntity<>(loginResponseDto, HttpStatus.OK);
 	}
 
-	@PostMapping("/logout")
+	@DeleteMapping("/logout")
 	@Operation(summary = "로그아웃")
 	public ResponseEntity<Void> logoutMember(HttpServletRequest request) {
 		HttpSession session = request.getSession(false); // 현재 세션 가져오기 (없으면 null 반환)
 
 		if (session != null) {
-			session.invalidate(); // 세션 무효화
+			memberService.logoutMember(session); // MemberService 호출
 			return new ResponseEntity<>(HttpStatus.NO_CONTENT); // 204 No Content
 		} else {
 			return new ResponseEntity<>(HttpStatus.UNAUTHORIZED); // 401 Unauthorized
 		}
 	}
+
+
+
 
 }

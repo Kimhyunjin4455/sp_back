fe3624b30b24b7c8e068651bcbac3f24acdc89a5
Kimhyunjin4455 <dp4455@naver.com>
2024-09-12 17:46:34 +0900

    fix: 할인 값 제한 설정은 관리자 역할이기에 코드 구현하지 않고 DB 테이블에 대해 수작업으로 값 제한하는 것이 옳다고 판단

--- src/main/java/starbucks3355/starbucksServer/domainProduct/entity/ProductDefaultDisCount.java
+++ src/main/java/starbucks3355/starbucksServer/domainProduct/entity/ProductDefaultDisCount.java
@@ -26,5 +26,20 @@ public class ProductDefaultDisCount {
 	@Column(nullable = false)
 	@Enumerated(EnumType.STRING)
 	private DiscountType discountType;
+	//일정 금액 만큼 할인값을 부여, 상품에 대해 적용할 때 상품 가격보다 더 큰값은 설정할 수 없음.
 	private int discountValue;
+
+	// public void inputDiscountValue(int discountValue, ProductDetails productDetails) {
+	// 	if (discountType == DiscountType.PERCENT) {
+	// 		if (discountValue < 0 || discountValue > 90) {
+	// 			throw new IllegalArgumentException("PERCENT discount must be between 0 and 90.");
+	// 		}
+	// 	} else if (discountType == DiscountType.AMOUNT) {
+	// 		double maxDiscountValue = productDetails.getProductPrice() * 0.9;
+	// 		if (discountValue < 0 || discountValue > maxDiscountValue) {
+	// 			throw new IllegalArgumentException("AMOUNT discount must not exceed 90% of the product price.");
+	// 		}
+	// 	}
+	// 	this.discountValue = discountValue;
+	// }
 }

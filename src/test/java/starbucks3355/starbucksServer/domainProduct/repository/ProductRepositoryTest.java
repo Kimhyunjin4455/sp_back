b232193f98a3793723cbb5f759e9a51685df24d3
Kimhyunjin4455 <dp4455@naver.com>
2024-09-27 09:52:05 +0900

    fix: 테스트 정보 주석 처리

--- src/test/java/starbucks3355/starbucksServer/domainProduct/repository/ProductRepositoryTest.java
+++ src/test/java/starbucks3355/starbucksServer/domainProduct/repository/ProductRepositoryTest.java
@@ -1,27 +1,22 @@
 package starbucks3355.starbucksServer.domainProduct.repository;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import lombok.extern.slf4j.Slf4j;
-
-@SpringBootTest
-@Slf4j
-public class ProductRepositoryTest {
-	@Autowired
-	private ProductRepository productRepository;
-
-	// @Test
-	// public void 상품더미데이터삽입() {
-	// 	IntStream.rangeClosed(1, 100000000).forEach(i -> {
-	// 		Product product = Product.builder()
-	// 			.productUuid(UUID.randomUUID().toString())
-	// 			.productName("c상품" + i)
-	// 			.productDescription("상품상세설명" + i)
-	// 			.productInfo("상품간단정보" + i)
-	// 			.build();
-	// 		productRepository.save(product);
-	// 	});
-
-}
+// @SpringBootTest
+// @Slf4j
+// public class ProductRepositoryTest {
+// 	@Autowired
+// 	private ProductRepository productRepository;
+//
+// 	// @Test
+// 	// public void 상품더미데이터삽입() {
+// 	// 	IntStream.rangeClosed(1, 100000000).forEach(i -> {
+// 	// 		Product product = Product.builder()
+// 	// 			.productUuid(UUID.randomUUID().toString())
+// 	// 			.productName("c상품" + i)
+// 	// 			.productDescription("상품상세설명" + i)
+// 	// 			.productInfo("상품간단정보" + i)
+// 	// 			.build();
+// 	// 		productRepository.save(product);
+// 	// 	});
+//
+// }
 
